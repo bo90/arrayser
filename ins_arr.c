d08b56c7b4761098fc12f7c9ed0b3fe6bb6b71df
user <bo90ris@gmail.com>
2024-01-06 12:00:27 +0300

    printList, delete and instructions implemetions methods

--- ins_arr.c
+++ ins_arr.c
@@ -53,17 +53,54 @@ void insert(ListNodePtr *sPtr, char value)
 }
 
 char delete (ListNodePtr *sPtr, char value){
+    ListNodePtr previousPtr;
+    ListNodePtr currentPtr;
+    ListNodePtr tempPtr;
 
+    if(value == (*sPtr)->data){
+        tempPtr = *sPtr;
+        *sPtr = (*sPtr);
+        free(tempPtr);
+        return value;
+    }else{
+        previousPtr = *sPtr;
+        currentPtr = (*sPtr)->nextPtr;
+
+        while (currentPtr != NULL && currentPtr->data != value){
+            previousPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+        }
+
+        if(currentPtr != NULL){
+            tempPtr = currentPtr;
+            previousPtr->nextPtr = currentPtr->nextPtr;
+            free(tempPtr);
+            return value;
+        }
+    }
+    return '\0';
 }
 
 int isEmpty(ListNodePtr sPtr) {
-    return 0;
+    return sPtr == NULL;
 }
 
 void printList(ListNodePtr currentPtr) {
-
+    if(isEmpty(currentPtr)){
+        puts("List is empty.\n");
+    }else{
+        puts("The list is: ");
+        while (currentPtr != NULL){
+            printf("%c --> ", currentPtr->data);
+            currentPtr = currentPtr->nextPtr;
+        }
+        puts("NULL\n");
+    }
 }
 
 void instructions() {
-
+    puts("Enter your choice:\n"
+         "1 to insert an element into the list.\n"
+         "2 to delete an element from the list.\n"
+         "3 to end.");
 }
