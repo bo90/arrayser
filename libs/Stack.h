4899d329225c1bc7f77a7acdc35105607fb02e8d
user <bo90ris@gmail.com>
2024-01-20 21:02:21 +0300

    add RandomItems in method

---
+++ libs/Stack.h
@@ -0,0 +1,13 @@
+//
+// Created by Борис Левченков on 25.01.2024.
+//
+#include "Item.h"
+#ifndef FIG12_03_STACK_H
+#define FIG12_03_STACK_H
+
+#endif //FIG12_03_STACK_H
+
+void STACKinit(int);
+int STACKempty();
+void STACKpush(Item);
+Item STACKpop();
