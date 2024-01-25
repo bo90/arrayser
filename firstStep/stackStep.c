0c86e731f485eafc7e013fe224635c932e4138d7
user <bo90ris@gmail.com>
2024-01-25 20:48:28 +0300

    add Stack steps

---
+++ firstStep/stackStep.c
@@ -0,0 +1,26 @@
+//
+// Created by Борис Левченков on 25.01.2024.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "libs/Item.h"
+#include "libs/Stack.h"
+
+int main(){
+    srand(time(NULL));
+    char a[] = {0};
+    int i, N;
+    N = 10;
+
+    for(i = 0; i < N; i++){
+        if(a[i] == '+'){
+            STACKpush(STACKpop());
+        }
+        if(a[i] == '*'){
+            STACKpush(STACKpop());
+        }
+    }
+}
